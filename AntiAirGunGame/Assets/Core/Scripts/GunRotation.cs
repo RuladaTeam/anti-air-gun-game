8d543be3dd7c6af75f586971fa2383df828f47ca
Wimpy57 <venigretdj@gmail.com>
2025-04-10 19:04:08 +0300

    Capsule pitch setting

--- AntiAirGunGame/Assets/Core/Scripts/GunRotation.cs
+++ AntiAirGunGame/Assets/Core/Scripts/GunRotation.cs
@@ -1,25 +1,59 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace Core.Scripts
 {
     public class GunRotation : MonoBehaviour
     {
+        [Header("Rotation")]
         [SerializeField] private Transform _rotationWheelTransform;
         [SerializeField] private Transform _rotatableTransform;
+        
+        [Header("Pitch")]
+        [SerializeField] private Transform _pitchWheelTransform;
+        [SerializeField] private Transform[] _pitchableTransforms;
+        
+        [Header("Coefficients")]
         [SerializeField] private int _rotationRatio = 20;
+        [SerializeField] private float _pitchRatio = 20;
         [SerializeField] private float _rotationRollCoefficient = .33f;
         [SerializeField] private float _rotationPitchCoefficient = 0;
+        [SerializeField] private float _pitchCoefficient = 0.02f;
         
         private Vector3 _previousEulerAngles;
         private Vector3 _totalRotation;
+        
+        private Vector3 _totalPitch;
+        private Vector3 _previousPitch;
 
+        private float _currentRotationWheelValue;
+        private float _currentPitchWheelValue;
+        
+        private float _totalCapsulePitchValue;
+        private float _totalCapsuleRollValue;
+        
         private void Start()
         {
             _previousEulerAngles = _rotationWheelTransform.localEulerAngles;
             _totalRotation = _previousEulerAngles;
+            
+            _previousPitch = _pitchWheelTransform.localEulerAngles;
+            _totalPitch = _previousPitch;
         }
         
         private void Update()
+        {
+            _currentPitchWheelValue = 0;
+            _currentRotationWheelValue = 0;
+            
+            HandlePitch();
+            HandleRotation();
+            
+            FutuRiftCapsuleController.Instance?.SetPitchAndRoll(
+                -_currentPitchWheelValue, _currentRotationWheelValue);
+        }
+
+        private void HandleRotation()
         {
             Vector3 currentEulerAngles = _rotationWheelTransform.localEulerAngles;
 
@@ -33,10 +67,34 @@ namespace Core.Scripts
             _previousEulerAngles = currentEulerAngles;
     
             _rotatableTransform.eulerAngles = new Vector3(
-                _totalRotation.x, _totalRotation.y/_rotationRatio, _totalRotation.z);
+                _rotatableTransform.eulerAngles.x, _totalRotation.y/_rotationRatio, _rotatableTransform.eulerAngles.z);
+
+            _currentRotationWheelValue += -deltaRotation.y * _rotationRollCoefficient;
+            _currentPitchWheelValue += -deltaRotation.y * _rotationPitchCoefficient;
+        }
+
+        private void HandlePitch()
+        {
+            if (_totalPitch.z/_pitchRatio >= 80) return;
             
-            FutuRiftCapsuleController.Instance?.SetPitchAndRoll(
-                -deltaRotation.y * _rotationPitchCoefficient, -deltaRotation.y * _rotationRollCoefficient);
+            Vector3 currentPitch = _pitchWheelTransform.localEulerAngles;
+
+            Vector3 deltaPitch = new Vector3(
+                DeltaAngle(_previousPitch.x, currentPitch.x),
+                DeltaAngle(_previousPitch.y, currentPitch.y),
+                DeltaAngle(_previousPitch.z, currentPitch.z)
+            );
+
+            _totalPitch += deltaPitch;
+            _previousPitch = currentPitch;
+
+            foreach (var pitchable in _pitchableTransforms)
+            {
+                pitchable.eulerAngles = new Vector3(
+                    pitchable.localEulerAngles.x, pitchable.localEulerAngles.y, _totalPitch.z/_pitchRatio);
+            }
+            
+            _currentPitchWheelValue += _totalPitch.z * _pitchCoefficient / 10;
         }
 
         private float DeltaAngle(float previousAngle, float currentAngle)

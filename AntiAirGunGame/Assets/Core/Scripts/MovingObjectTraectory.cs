5c6f4716257622819b80283bff9915f3c896fab5
quq-quq <106841555+quq-quq@users.noreply.github.com>
2025-04-15 20:19:52 +0300

    Bullet

--- AntiAirGunGame/Assets/Core/Scripts/MovingObjectTraectory.cs
+++ AntiAirGunGame/Assets/Core/Scripts/MovingObjectTraectory.cs
@@ -3,46 +3,42 @@ using UnityEngine;
 using DG;
 using DG.Tweening;
 
-[ExecuteAlways]
+
 public class MovingObjectTraectory : MonoBehaviour
 {
     [Header("Trajectory Settings")]
-    [SerializeField] private Transform _startTransform;
-    [SerializeField] private Transform _endTransform;
+    [SerializeField] protected Transform _startTransform;
+    [SerializeField] protected Transform _endTransform;
     [SerializeField] private float _height = 2f;
     [Space(10)]
     [SerializeField] protected int _segments = 20;
     [Space(30)]
     [SerializeField, Min(0.1f)] private float _duration;
-    protected Vector3 _startPoint;
-    protected Vector3 _endPoint;
-    //private Vector3[] _segmetPoints;
+    private Vector3[] _segmetPoints;
 
-    //protected void MovingObj(Transform objTransform, PathType pathType = PathType.Linear)
-    //{
-    //    objTransform.DOPath(_segmetPoints, _duration, pathType).OnComplete(() =>
-    //    {
-    //        Destroy(objTransform.gameObject);
-    //    });
-    //}
-
-    private void OnDrawGizmos()
+    protected void MovingObj(Transform objTransform, PathType pathType = PathType.Linear)
     {
-        
+        objTransform.DOPath(_segmetPoints, _duration, pathType).OnComplete(() =>
+        {
+            Destroy(objTransform.gameObject);
+        });
+    }
+
+    protected void OnDrawGizmos()
+    {
         Gizmos.color = Color.red;
-        Gizmos.DrawSphere(_startPoint, 0.5f);
-        Gizmos.DrawSphere(_endPoint, 0.5f);
+        Gizmos.DrawSphere(_startTransform.position, 0.1f);
+        Gizmos.DrawSphere(_endTransform.position, 0.5f);
 
-        Vector3 topPoint = CalculateParabolaPoint(0.5f, _startPoint, _endPoint, _height);
+        Vector3 topPoint = CalculateParabolaPoint(0.5f, _startTransform.position, _endTransform.position, _height);
         Gizmos.DrawSphere(topPoint, 0.5f);
-
-        Vector3 previousPoint = _startPoint;
-        //_segmetPoints = new Vector3[_segments+1];
-        for (int i = 1; i < _segments+1; i++)
+        Vector3 previousPoint = _startTransform.position;
+        _segmetPoints = new Vector3[_segments];
+        for (int i = 1; i <= _segments; i++)
         {
             float t = i / (float)_segments;
-            Vector3 currentPoint = CalculateParabolaPoint(t, _startPoint, _endPoint, _height);
-            //_segmetPoints[i] = currentPoint;
+            Vector3 currentPoint = CalculateParabolaPoint(t, _startTransform.position, _endTransform.position, _height);
+            _segmetPoints[i-1] = currentPoint;
             Gizmos.DrawLine(previousPoint, currentPoint);
             previousPoint = currentPoint;
         }
--- AntiAirGunGame/Assets/Core/Scripts/Shooting/BulletTraectory.cs
+++ AntiAirGunGame/Assets/Core/Scripts/Shooting/BulletTraectory.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using DG.Tweening;
 
-[ExecuteAlways]
+
 public class BulletTraectory : MovingObjectTraectory
 {
     [SerializeField] private Transform _gunTransform;
@@ -10,44 +10,50 @@ public class BulletTraectory : MovingObjectTraectory
     private bool _isShooting;
     private float _currentOfsetTime;
 
-    private void Update()
+    private new void OnDrawGizmos()
     {
-        //Shoot();
+        base.OnDrawGizmos();
+        Gizmos.color = Color.white;
+        Gizmos.DrawLine(_startTransform.position, new Vector3(_endTransform.position.x, CalculateLineYPos(_startTransform.position, _gunTransform.position, _endTransform.position), _endTransform.position.z));
+
+    }
+    private void Start()
+    {
+        _isShooting = true;
+        _currentOfsetTime = _ofsetTime;
     }
 
-    private void OnDrawGizmos()
+    private void Update()
     {
-        //base.OnDrawGizmos();
-        Gizmos.color = Color.white;
-        Gizmos.DrawLine(_startPoint, new Vector3(_endPoint.x, CalculateLineYPos(_startPoint, _gunTransform.position, _endPoint), _endPoint.z));
 
+        Shoot();
+
+        if (_currentOfsetTime > 0)
+        {
+            _currentOfsetTime -= Time.deltaTime;
+        }
+        else
+        {
+            _isShooting = false;
+        }
     }
 
     private float CalculateLineYPos(Vector3 startPoint, Vector3 gunPoint, Vector3 endPoint)
     {
-        return ((endPoint.x - startPoint.x)/(gunPoint.x - startPoint.x))*(gunPoint.y - startPoint.y) + startPoint.y;
+        return ((endPoint.x - startPoint.x) / (gunPoint.x - startPoint.x)) * (gunPoint.y - startPoint.y) + startPoint.y;
     }
 
-    //private void Shoot()
-    //{
-    //    if (!_isShooting)
-    //    {
-    //        GameObject currentBullet = Instantiate(_bullet, _gunTransform);
-    //        MovingObj(currentBullet.transform);
-    //        _isShooting = true;
-    //        _currentOfsetTime = _ofsetTime;
-    //    }
-    //}
+    private void Shoot()
+    {
+        if (!_isShooting)
+        {
+            Debug.Log("1");
+            GameObject currentBullet = Instantiate(_bullet, _startTransform.position, _gunTransform.rotation);
+            MovingObj(currentBullet.transform);
+            _isShooting = true;
+            _currentOfsetTime = _ofsetTime;
+        }
+    }
 
-    //private void FixedUpdate()
-    //{
-    //    if (_currentOfsetTime>0)
-    //    {
-    //        _currentOfsetTime -= Time.deltaTime;
-    //    }
-    //    else
-    //    {
-    //        _isShooting = false;
-    //    }
-    //}
+
 }
